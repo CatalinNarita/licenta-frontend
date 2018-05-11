972903067609413ec1220230b00079d047282594
Catalin Narita <catalin.ioan.narita@gmail.com>
2018-05-11 13:29:16 +0300

    check user role before requesting token

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {LoginService} from './login.service';
 import {Router} from '@angular/router';
 
-const BAD_CREDENTIALS_RESPONSE_CODE = 400;
+const BAD_CREDENTIALS_RESPONSE_CODE = 500;
 
 @Component({
   selector: 'app-login',
@@ -24,22 +24,34 @@ export class LoginComponent {
 
   }
 
-  requestAccessToken() {
+  doLogin() {
     this.loading = true;
-    this.loginService.requestAccessToken(this.username, this.password)
+    this.loginService.checkUserRole(this.username)
+      .subscribe(data => {
+        if (data.roleType === 'ROLE_ADMIN') {
+          this.requestAccessToken(this.username, this.password);
+        } else {
+          this.loading = false;
+          this.invalidCredentials = true;
+        }
+      },
+        error => {
+        if (error.status === BAD_CREDENTIALS_RESPONSE_CODE) {
+            console.log(error);
+            this.loading = false;
+            this.invalidCredentials = true;
+          }
+        });
+  }
+
+  requestAccessToken(username: string, password: string) {
+    this.loginService.requestAccessToken(username, password)
       .subscribe(
         data => {
           this.invalidCredentials = false;
           localStorage.setItem('access_token', data.access_token);
           this.redirectUser();
           this.loading = false;
-        },
-        error => {
-          if (error.status === BAD_CREDENTIALS_RESPONSE_CODE) {
-            console.log(error);
-            this.loading = false;
-            this.invalidCredentials = true;
-          }
         });
   }
 
--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -4,6 +4,7 @@ import {Observable} from 'rxjs';
 import {BASE_URL} from '../app.component';
 
 const REQUEST_TOKEN_URL = BASE_URL + '/oauth/token';
+const CHECK_USER_ROLE_URL = BASE_URL + '/user/checkUserRole/';
 
 @Injectable()
 export class LoginService {
@@ -27,6 +28,10 @@ export class LoginService {
     return new HttpHeaders()
       .set('Authorization', 'Basic ' + btoa('android-oauth2-client-id:android-oauth2-client-pass'))
       .set('Content-Type', 'application/x-www-form-urlencoded');
+  }
+
+  checkUserRole(username: string): Observable<any> {
+    return this.http.get(CHECK_USER_ROLE_URL + username);
   }
 
 }
